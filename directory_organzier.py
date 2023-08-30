9e5416ab073eb73588abde9ab68eb772a655c71d
Eduard-Valentin Cinca <eduard_cinca@yahoo.com>
2023-08-30 17:57:37 +0300

    Directory organizer. A script that takes a folder and organise all the items in subfolders sorted by the item type. Created the structure of the script.

---
+++ directory_organzier.py
@@ -0,0 +1,20 @@
+def menu():
+    pass
+
+def path_validation():
+    pass
+
+def create_directories():
+    pass
+
+def list_all_files():
+    pass
+
+def get_file_extension():
+    pass
+
+def map_extension_to_folder():
+    pass
+
+
+
